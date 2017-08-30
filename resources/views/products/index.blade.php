a1c0c8bd56ab7f5db0aa27e81be0ac04d255f2cd
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-30 16:45:35 +0700

    Auth::check() to check log in already or not yet to show data

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 @section('content')
 
@@ -11,6 +12,7 @@
             </div>
             {{--content--}}
             <div class="col-sm-9 col-sm-9">
+                @if(Auth::check())
                 @foreach($products as $product)
                 <div class="col-sm-6 col-md-4" style="padding-left: 5px; padding-right: 5px">
                     <div class="products">
@@ -29,10 +31,13 @@
                             </form>
                         </div>
                     </div>
-
                 </div>
                     @endforeach
+                    @else
+                        <div class="alert alert-danger">Log-in to see products</div>
+                    @endif
             </div>
         </div>
     </div>
 @endsection
+
