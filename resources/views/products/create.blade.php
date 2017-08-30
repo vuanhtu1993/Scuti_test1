fc963ebd22fb8c0a5676688a20d7fdb17de0f841
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-30 11:27:11 +0700

    create product

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -8,13 +8,13 @@
                 <li><a href="{{route('products.index')}}">List</a></li>
             </ul>
         </div>
-        {{--content--}}
+        {{--form--}}
         <div class="col-sm-9 col-sm-9">
             <form class="form-group" action="{{route('products.store')}}" method="post" enctype="multipart/form-data" style="width: 100%">
                 {{csrf_field()}}
                 Name Product: <input class="form-control" type="text" name="name" value="">
                 Price: <input class="form-control" type="text" name="price" value="">
-                Description:<input class="form-control" type="text" name="description" value="">
+                Description:<input style="height: 100px;" class="form-control" type="text" name="description" value="">
                 <br>
                 Select the file to upload
                 <input class="form-inline"  type="file" name="imgUrl" value="">
